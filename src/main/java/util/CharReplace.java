ce2e7d222818e4c6105e3b1e05001fdb3afdf185
mengfeifei <happymff@126.com>
2017-12-04 22:58:44 +0800

    add http simple reques method

--- src/main/java/util/CharReplace.java
+++ src/main/java/util/CharReplace.java
@@ -4,7 +4,7 @@ package util;
  * Created by guojianhong on 2017/8/11.
  */
 public class CharReplace {
-
+//This is the Test
     public static String  getCharReplace(String str){
         if(str.contains("\"")){
             String strnew = str.replace("\"","");
